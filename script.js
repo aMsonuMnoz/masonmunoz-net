49998584344a5a59030ba706c5c4d922ed58c1d0
aMsonuMnoz <114356675+aMsonuMnoz@users.noreply.github.com>
2025-01-25 13:54:41 -0600

    construction + nav UI

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
+let id = 'home';
+document.getElementById(id).classList.add('current');
 function loadContent(page) {
+    console.log(id);
+    document.getElementById(id).classList.remove('current');
     fetch(page)
         .then(response => response.text())
         .then(data => {
             document.getElementById('content').innerHTML = data;
         })
         .catch(error => console.error('Error loading content:', error));
+    id = page.slice(5, page.length-5);
+    document.getElementById(id).classList.add('current');
 }
